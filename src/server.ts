6f3b16df81269543eafe032f4ef96440947fb6a1
Leo Dantas <35945849-leodj030603@users.noreply.replit.com>
2024-10-31 00:51:43 +0000

    refactor:adicionando migrations

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts']
-};
+import express from 'express';
+import pool from './config/database';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
